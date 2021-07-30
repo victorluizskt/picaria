0b6b34af59e7de6e3622498b6d5cad4dcd39a2eb
Victor Luiz <victorluizcefet@gmail.com>
2021-07-29 21:18:49 -0300

    Refazendo o projeto

--- Picaria/Picaria.h
+++ Picaria/Picaria.h
@@ -4,26 +4,63 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Picaria; }
+namespace Ui {
+    class Picaria;
+}
 QT_END_NAMESPACE
 
 class Hole;
 
 class Picaria : public QMainWindow {
     Q_OBJECT
+    Q_PROPERTY(Picaria::Mode mode READ mode WRITE setMode NOTIFY modeChanged)
 
 public:
+    enum Mode {
+        NineHoles,
+        ThirteenHoles
+    };
+    Q_ENUM(Mode)
+
+    enum Player {
+        RedPlayer,
+        BluePlayer
+    };
+    Q_ENUM(Player)
+
+    enum Phase {
+        DropPhase,
+        MovePhase
+    };
+    Q_ENUM(Phase)
+
     Picaria(QWidget *parent = nullptr);
-    ~Picaria();
+    virtual ~Picaria();
+
+    Picaria::Mode mode() const { return m_mode; }
+    void setMode(Picaria::Mode mode);
+
+signals:
+    void modeChanged(Picaria::Mode mode);
 
 private:
     Ui::Picaria *ui;
-    Hole* m_holes[9];
+    Hole* m_holes[13];
+    Mode m_mode;
+    Player m_player;
+    Phase m_phase;
+
+    void switchPlayer();
 
 private slots:
     void play(int id);
     void reset();
+
     void showAbout();
+
+    void updateMode(QAction* action);
+    void updateStatusBar();
+
 };
 
 #endif // PICARIA_H
--- build-Picaria-Desktop_Qt_5_12_11_MinGW_32_bit-Debug/debug/moc_Picaria.cpp
+++ build-Picaria-Desktop_Qt_5_12_11_MinGW_32_bit-Debug/debug/moc_Picaria.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Picaria_t {
-    QByteArrayData data[6];
-    char stringdata0[33];
+    QByteArrayData data[22];
+    char stringdata0[190];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,14 +32,34 @@ struct qt_meta_stringdata_Picaria_t {
 static const qt_meta_stringdata_Picaria_t qt_meta_stringdata_Picaria = {
     {
 QT_MOC_LITERAL(0, 0, 7), // "Picaria"
-QT_MOC_LITERAL(1, 8, 4), // "play"
-QT_MOC_LITERAL(2, 13, 0), // ""
-QT_MOC_LITERAL(3, 14, 2), // "id"
-QT_MOC_LITERAL(4, 17, 5), // "reset"
-QT_MOC_LITERAL(5, 23, 9) // "showAbout"
+QT_MOC_LITERAL(1, 8, 11), // "modeChanged"
+QT_MOC_LITERAL(2, 20, 0), // ""
+QT_MOC_LITERAL(3, 21, 13), // "Picaria::Mode"
+QT_MOC_LITERAL(4, 35, 4), // "mode"
+QT_MOC_LITERAL(5, 40, 4), // "play"
+QT_MOC_LITERAL(6, 45, 2), // "id"
+QT_MOC_LITERAL(7, 48, 5), // "reset"
+QT_MOC_LITERAL(8, 54, 9), // "showAbout"
+QT_MOC_LITERAL(9, 64, 10), // "updateMode"
+QT_MOC_LITERAL(10, 75, 8), // "QAction*"
+QT_MOC_LITERAL(11, 84, 6), // "action"
+QT_MOC_LITERAL(12, 91, 15), // "updateStatusBar"
+QT_MOC_LITERAL(13, 107, 4), // "Mode"
+QT_MOC_LITERAL(14, 112, 9), // "NineHoles"
+QT_MOC_LITERAL(15, 122, 13), // "ThirteenHoles"
+QT_MOC_LITERAL(16, 136, 6), // "Player"
+QT_MOC_LITERAL(17, 143, 9), // "RedPlayer"
+QT_MOC_LITERAL(18, 153, 10), // "BluePlayer"
+QT_MOC_LITERAL(19, 164, 5), // "Phase"
+QT_MOC_LITERAL(20, 170, 9), // "DropPhase"
+QT_MOC_LITERAL(21, 180, 9) // "MovePhase"
 
     },
-    "Picaria\0play\0\0id\0reset\0showAbout"
+    "Picaria\0modeChanged\0\0Picaria::Mode\0"
+    "mode\0play\0id\0reset\0showAbout\0updateMode\0"
+    "QAction*\0action\0updateStatusBar\0Mode\0"
+    "NineHoles\0ThirteenHoles\0Player\0RedPlayer\0"
+    "BluePlayer\0Phase\0DropPhase\0MovePhase"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,22 +69,51 @@ static const uint qt_meta_data_Picaria[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
-       0,    0, // properties
-       0,    0, // enums/sets
+       6,   14, // methods
+       1,   56, // properties
+       3,   60, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   44,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x08 /* Private */,
-       4,    0,   32,    2, 0x08 /* Private */,
-       5,    0,   33,    2, 0x08 /* Private */,
+       5,    1,   47,    2, 0x08 /* Private */,
+       7,    0,   50,    2, 0x08 /* Private */,
+       8,    0,   51,    2, 0x08 /* Private */,
+       9,    1,   52,    2, 0x08 /* Private */,
+      12,    0,   55,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    6,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 10,   11,
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       4, 0x80000000 | 3, 0x0049510b,
+
+ // properties: notify_signal_id
+       0,
+
+ // enums: name, alias, flags, count, data
+      13,   13, 0x0,    2,   75,
+      16,   16, 0x0,    2,   79,
+      19,   19, 0x0,    2,   83,
+
+ // enum data: key, value
+      14, uint(Picaria::NineHoles),
+      15, uint(Picaria::ThirteenHoles),
+      17, uint(Picaria::RedPlayer),
+      18, uint(Picaria::BluePlayer),
+      20, uint(Picaria::DropPhase),
+      21, uint(Picaria::MovePhase),
 
        0        // eod
 };
@@ -75,12 +124,44 @@ void Picaria::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         auto *_t = static_cast<Picaria *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->play((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 1: _t->reset(); break;
-        case 2: _t->showAbout(); break;
+        case 0: _t->modeChanged((*reinterpret_cast< Picaria::Mode(*)>(_a[1]))); break;
+        case 1: _t->play((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->reset(); break;
+        case 3: _t->showAbout(); break;
+        case 4: _t->updateMode((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        case 5: _t->updateStatusBar(); break;
         default: ;
         }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (Picaria::*)(Picaria::Mode );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Picaria::modeChanged)) {
+                *result = 0;
+                return;
+            }
+        }
     }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<Picaria *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< Picaria::Mode*>(_v) = _t->mode(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<Picaria *>(_o);
+        Q_UNUSED(_t)
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setMode(*reinterpret_cast< Picaria::Mode*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    }
+#endif // QT_NO_PROPERTIES
 }
 
 QT_INIT_METAOBJECT const QMetaObject Picaria::staticMetaObject = { {
@@ -112,15 +193,39 @@ int Picaria::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 6;
     }
+#ifndef QT_NO_PROPERTIES
+    else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 1;
+    }
+#endif // QT_NO_PROPERTIES
     return _id;
+}
+
+// SIGNAL 0
+void Picaria::modeChanged(Picaria::Mode _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
